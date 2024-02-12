6016eb9021f5f89158ee2fd514ec6b55098b3778
ponchik18 <maks.bovkun.228.111@gmail.com>
2024-02-12 18:21:48 +0300

    Initial commit

---
+++ src/main/java/com/bsuir/FreelanceExchangesApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceExchangesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceExchangesApplication.class, args);
+    }
+
+}
