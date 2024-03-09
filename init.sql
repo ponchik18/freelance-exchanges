8f03d10faf38bafeadcdcf1da789a815170a9d7b
ponchik18 <maks.bovkun.228.111@gmail.com>
2024-03-09 12:41:18 +0300

    create base structure

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE customer_service
+       AUTHORIZATION postgres;
+CREATE DATABASE freelance_service
+       AUTHORIZATION postgres;
+CREATE DATABASE job_service
+       AUTHORIZATION postgres;
+CREATE DATABASE payment_service
+       AUTHORIZATION postgres;
+CREATE DATABASE rating_service
+       AUTHORIZATION postgres;
+CREATE DATABASE chat_service
+       AUTHORIZATION postgres;
